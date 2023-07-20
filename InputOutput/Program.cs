dac7ec04d582ed3d51b1aba41dd1fc81435dcfda
mahfuz khan <mahfuzpust@gmail.com>
2023-07-20 11:07:19 +0600

    string to convert int-float-double

--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -1,21 +1,18 @@
-﻿Console.WriteLine("Mahfuz");
-Console.WriteLine("Mahfuz");
+﻿
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is: " + age);
 
-Console.Write("Mahfuz");
-Console.Write("Mahfuz");
-Console.WriteLine();
+Console.WriteLine("Enter float number");
+float value = Single.Parse(Console.ReadLine());
+Console.WriteLine($"The float value is: {value}");
 
+Console.WriteLine("Enter float value:");
+float value2 = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine($"The outpust : {value2}");
 
-string line = Console.ReadLine();   
-Console.WriteLine($"The value is:{line}");
-
-int a = Console.Read();
-
-char c = (char)a;
-
-int b = 5;
-
-Console.ReadKey();
-
+Console.WriteLine("Enter Double value:");
+double value3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"The double value is: {value3}");
 
 
