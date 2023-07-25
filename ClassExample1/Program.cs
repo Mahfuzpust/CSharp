e02d3690af504bd899bcedd33b77755e1beb26d8
mahfuz khan <mahfuzpust@gmail.com>
2023-07-25 23:48:48 +0600

    Class

---
+++ ClassExample1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+WaterBottle bottle1 = new WaterBottle(1000, "Red");
+bottle1.AddWater(100);
+
+Console.WriteLine(bottle1.Capacity);
+Console.WriteLine(bottle1.Color);
+
+WaterBottle bottle2 = new WaterBottle(2000, "Blue");
+bottle2.AddWater(200);
+
+Console.WriteLine(bottle2.Capacity);
+Console.WriteLine(bottle2.Color);
+
+
+
+
---
+++ ClassExample1/WaterBottle.cs
@@ -0,0 +1,42 @@
+﻿class WaterBottle
+{
+    private double _capacity;
+    private string _color;
+    public double waterAmount;
+
+    public double Capacity
+    {
+        get
+        {
+            return _capacity;
+        }
+        set
+        {
+            _capacity = value;
+        }
+    }
+    
+
+    public string Color
+    {
+        get
+        {
+            return _color;
+        }
+        set
+        {
+            _color = value;
+        }
+    }
+
+    public WaterBottle(double capacity, string color)
+    {
+        _capacity = capacity;
+        _color = color;
+    }
+
+    public void AddWater(int Amount)
+    {
+        _capacity += Amount;
+    }
+}
