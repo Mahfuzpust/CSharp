ed0c86b481c61733445d36f6cd0aba719cc9d319
mahfuz khan <mahfuzpust@gmail.com>
2023-07-28 00:04:18 +0600

    Static

---
+++ Static/EmailUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    public static class EmailUtility
+    {
+        //static used before variable
+        public static int count;
+
+        //static used before Property
+        public static double Length { get; set; }
+        //static used before Coonstructor
+        static EmailUtility()
+        {
+
+        }
+        //static used before Method
+        public static void SendEmail(string name, string email,
+            string subject, string body)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using Static;
+
+EmailUtility.count = 0;
+EmailUtility.Length = 0;
+EmailUtility.SendEmail("Mahfuz", "Mahfuzpust@gmail.com",
+    "C# Video", "Welcome");
