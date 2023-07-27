e7322cdbac12651fbc1114c69e0b14a2ae55f3fc
mahfuz khan <mahfuzpust@gmail.com>
2023-07-27 23:10:50 +0600

    Inheritance

---
+++ Inheritance/Cemera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Cemera : Electronics
+    {
+        public double ZoomCapacity { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"{Name}");
+            Console.WriteLine($"{Model}");
+            Console.WriteLine($"{ZoomCapacity}");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Inheritance;
+
+Cemera cemera = new Cemera();
+
+cemera.Name = "Cemera";
+cemera.Model = "Cannon";
+cemera.ZoomCapacity = 10;
+
+cemera.Display();
