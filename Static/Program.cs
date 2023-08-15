a573544d01d21e71b9d8b578decacd25cecc734c
mahfuz khan <mahfuzpust@gmail.com>
2023-08-15 21:42:18 +0600

    new

--- Static/Program.cs
+++ Static/Program.cs
@@ -3,6 +3,6 @@ using Static;
 
 EmailUtility.count = 0;
 EmailUtility.Length = 0;
-EmailUtility.SendEmail("Mahfuz", 
+EmailUtility.SendEmail("Khan", 
     "Mahfuzpust@gmail.com",
     "C# Video", "text");
